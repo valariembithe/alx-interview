dc7d40a7286a7a57d9938e6fdcd284cb1ad424d3
valariembithe <valmbithe691@gmail.com>
2024-02-08 12:00:29 +0300

    Tasks 0

--- 0x05-nqueens/0-nqueens.py
+++ 0x05-nqueens/0-nqueens.py
@@ -1,34 +1,56 @@
 #!/usr/bin/python3
-'''N Queens'''
+"""N queens solution finder module.
+"""
 import sys
 
 
 solutions = []
+"""The list of possible solutions to the N queens problem.
+"""
 n = 0
+"""The size of the chessboard.
+"""
 pos = None
+"""The list of possible positions on the chessboard.
+"""
 
 
 def get_input():
-    ''' Raises error'''
+    """Retrieves and validates this program's argument.
+
+    Returns:
+        int: The size of the chessboard.
+    """
     global n
     n = 0
     if len(sys.argv) != 2:
-        print('Usage: nqueens N')
+        print("Usage: nqueens N")
         sys.exit(1)
     try:
-        n = int(sys.argv(1))
+        n = int(sys.argv[1])
     except Exception:
-        print('N must be a number')
+        print("N must be a number")
         sys.exit(1)
     if n < 4:
-        print('N must be at least 4')
+        print("N must be at least 4")
         sys.exit(1)
+    return n
 
-def is_attacking(pos_0, pos_1):
-    '''Checks if position of queens is in attacking mode'''
-    if (pos_0[0] == pos_1[0] or pos_0[1] == pos_1[1]):
+
+def is_attacking(pos0, pos1):
+    """Checks if the positions of two queens are in an attacking mode.
+
+    Args:
+        pos0 (list or tuple): The first queen's position.
+        pos1 (list or tuple): The second queen's position.
+
+    Returns:
+        bool: True if the queens are in an attacking position else False.
+    """
+    if (pos0[0] == pos1[0]) or (pos0[1] == pos1[1]):
         return True
-    return abs(pos_0[0] - pos_1[0]) == abs(pos_0[1] - pos_1[1])
+    return abs(pos0[0] - pos1[0]) == abs(pos0[1] - pos1[1])
+
 
 def group_exists(group):
     """Checks if a group exists in the list of solutions.
@@ -49,6 +71,7 @@ def group_exists(group):
         if i == n:
             return True
     return False
+
 
 def build_solution(row, group):
     """Builds a solution for the n queens problem.
